040f5b21b447478e444fb2ceb922fb561f4ff7d6
Никита <@LAPTOP-NKB>
2023-11-14 03:22:26 +0300

    Добавьте файлы проекта.

---
+++ P.NET_0.1.0/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using P.NET_.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_NET.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+
+        public RelayCommand  HomeViewCommand { get; set; }
+        public RelayCommand ServersViewCommand { get; set; }
+        public RelayCommand AccountViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+        public RelayCommand RegistrationViewCommand { get; set; }
+        public RelayCommand LoginViewCommand { get; set; }
+        public HomeViewModel HomeVm { get; set; }
+        public ServersViewModel ServersVm { get; set; }
+        public AccountViewModel AccountVm { get; set; }
+        public SettingsViewModel SettingsVm { get; set; }
+        public RegistrationViewModel RegistrationVm { get; set; }
+        public LoginViewModel LoginVm { get; set; }
+
+        private object _currentView;
+        public object CurrentView 
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnProperyChanged();
+            } 
+        }
+        public MainViewModel() 
+        {
+            HomeVm = new HomeViewModel();
+            ServersVm = new ServersViewModel();
+            AccountVm = new AccountViewModel();
+            SettingsVm = new SettingsViewModel();
+            RegistrationVm = new RegistrationViewModel();
+            LoginVm = new LoginViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVm;
+            
+            });
+
+            ServersViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ServersVm;
+
+            });
+
+            AccountViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = AccountVm;
+
+            });
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVm;
+
+            });
+            RegistrationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RegistrationVm;
+
+            });
+            LoginViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = LoginVm;
+
+            });
+        }
+    }
+}
---
+++ P.NET_0.1.0/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using P.NET_;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P.NET_0._1._0
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+
+       
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BT_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        //private void BT_login_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var db = new P_NET_Entities();
+        //    var USER_INFO = new USER_INFO();
+
+        //    if (TB_login.Text != null && TB_password.Text != null)
+        //    {
+        //        foreach (var item in db.USER_INFO.ToArray())
+        //        {
+        //            if (TB_login.Text != item.username && TB_password.Text != item.password)
+        //            {
+        //                LB_login_or_registrate.Content = "Error password or login";
+        //            }
+        //            else 
+        //            {
+
+        //            }
+        //        }
+        //    }
+
+
+        //}
+
+        //private void BT_registration_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var db = new P_NET_Entities();
+        //    var USER_INFO = new USER_INFO();
+
+
+        //    if (TB_login.Text != null && TB_password.Text != null)
+        //    {
+        //        foreach (var item in db.USER_INFO.ToArray())
+        //        {
+        //            USER_INFO.id = item.id + 1;
+        //        }
+        //        USER_INFO.username = TB_login.Text;
+        //        USER_INFO.password = TB_password.Text;
+        //        db.USER_INFO.Add(USER_INFO);
+        //        db.SaveChanges();
+
+        //        TB_password.Text = "";
+        //        TB_login.Text = "";
+
+        //        LB_login_or_registrate.Content = "Login";
+
+
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("enter error");
+        //    }
+        //}
+
+
+    }
+}
